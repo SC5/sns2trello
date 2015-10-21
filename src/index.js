2f4df53cf87ce595390bcb8099ab38ff615b9774
Mikael Puittinen <mikael@iki.fi>
2015-10-21 13:05:05 +0300

    Initial commit for sns2trello

--- src/index.js
+++ src/index.js
@@ -8,22 +8,53 @@
  * @apiSuccess {String} OutputKey 
  */
 
-var slack = require('./slack');
+var Trello = require('node-trello');
+var config = require('./config.json');
+var trelloClient = new Trello(config.key, config.token);
+
+function getList(boardId, listId, cb) {
+    if (listId) {
+        cb(null, listId);
+    }
+    trelloClient.get('/1/boards/' + boardId + '/lists', function (err, data) {
+        if (err) {
+            cb(err);
+        }
+        if (!data || (data.length === 0)) {
+            cb('ERROR: No lists for board ' + trelloBoardId);
+        }
+        cb(null, data[0].id);
+    });
+}
 
 module.exports = exports = {
     handler : function(event, context) {
         var postEvent = JSON.parse(event.Records[0].Sns.Message);
-        var channel = postEvent.channel;
-        var message = postEvent.message;
-        var iconEmoji = postEvent.iconEmoji;
-        var senderName = postEvent.senderName;
-        var attachments = postEvent.attachments;
+        var trelloBoardId = postEvent.trelloBoardId;
+        var trelloListId = postEvent.trelloListId;
 
-        slack.sendMessage(channel, 
-            message, 
-            iconEmoji, 
-            senderName, 
-            attachments)
-        .then(context.succeed, context.fail);
+        if (! trelloBoardId) {
+            return context.fail('ERROR: trelloBoardId missing');
+        }
+        if (! postEvent.cardName) {
+            return context.fail('ERROR: card does not have a name');
+        }
+
+        getList(trelloBoardId, trelloListId, function(err, listId) {
+            var newCardData = {
+                name: postEvent.cardName,
+                desc: postEvent.cardDescription,
+                idList: listId,
+                pos: 'bottom', // Add card to the bottom to support FIFO process
+                due: postEvent.dueDate,
+                urlSource: null
+            };
+            trelloClient.post('/1/cards/', newCardData, function(err, data) {
+                if (err) {
+                    return context.fail(err);
+                }
+                context.succeed(data);
+            });
+        });
     }
 };
--- tests/test_function.js
+++ tests/test_function.js
@@ -4,15 +4,44 @@ var expect = require('expect'),
 
 lambda.init(lambdaFunc);
 
+var testBoardId = 'HbwtYXbD';
+
+function tomorrow() {
+    // Calculate due date
+    var dueDate = new Date();
+    dueDate.setHours(dueDate.getHours() + 24)
+
+    return dueDate;
+}
+
 describe('sns2slack', function() {
     this.timeout(10000);
-    it('Send a message to a group', function(done) {
+    it('Returns error if trelloBoardId is missing', function(done) {
         lambda.run({ Records: [ 
             { Sns: 
                 { Message: JSON.stringify({
-                    channel: '#sns2slack',
-                    message: 'Test Message',
-                    senderName: 'sns2slack tests'
+                    cardName: 'sns2slack ' + new Date(),
+                    carDescription: 'foobar',
+                    dueDate: tomorrow()
+                })}
+            }
+        ]}, function(error, response) {
+            if (error) {
+                expect(error).toMatch(/trelloBoardId/);
+                return done();
+            }
+            done('EXPECTED ERROR');
+        });
+    });
+
+    it('Create a card in Trello', function(done) {
+        lambda.run({ Records: [ 
+            { Sns: 
+                { Message: JSON.stringify({
+                    trelloBoardId: testBoardId,
+                    cardName: 'sns2slack ' + new Date(),
+                    carDescription: 'foobar',
+                    dueDate: tomorrow()
                 })}
             }
         ]}, function(error, response) {
